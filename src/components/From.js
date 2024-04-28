fe033fb0259f0fb9196a8f4081426d27b08e65c3
mofany-j <jiangminghui220@163.com>
2024-04-28 18:35:27 +0800

    回调函数与事件

--- src/components/From.js
+++ src/components/From.js
@@ -1,16 +1,34 @@
 import React from "react";
 
-function From(props){
+function From(props) {
 
-    return(
-        <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">What needs to be done?</label>
-        </h2>
-        <input type="text" id="new-todo-input" className="input input__lg" name="text" autoComplete="off"/>
-        <button type="submit" className="btn btn__primary btn__lg">Add</button>
-      </form>
-    );
+  console.log(props);
+
+  // 事件处理
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask("Say hello!");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default From;
